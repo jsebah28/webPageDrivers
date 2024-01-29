489b19fb1a5afa02de2bc9eee663ee29cb167520
jsebah28 <106888673+jsebah28@users.noreply.github.com>
2024-01-29 09:06:19 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function agregarTooltip(elementoId, textoTooltip) {
 
   contenedor.addEventListener('mousemove', (event) => {
     tooltip.style.display = 'block';
-    tooltip.style.left = event.pageX + 10 + 'px';
-    tooltip.style.top = event.pageY + 10 + 'px';
+    tooltip.style.left = event.pageX + 10 +'px';
+    tooltip.style.top = event.pageY + 10 +'px';
   });
 
   contenedor.addEventListener('mouseout', () => {
@@ -17,5 +17,20 @@ function agregarTooltip(elementoId, textoTooltip) {
   });
 }
 
-agregarTooltip('elemento1', 'Ver información del conductor');
-agregarTooltip('elemento2', 'Eliminar conductor');
+agregarTooltip('elemento1', 'Ir a la información del conductor');
+agregarTooltip('elemento2', 'Desvincular conductor');
+
+/* modificar fondo */
+document.getElementById('cambiarImagen').addEventListener('click', function() {
+  // Obtén el elemento al que deseas cambiar la imagen de fondo
+  var miElemento = document.getElementById('body');
+  // Cambia la propiedad background-image
+  miElemento.style.backgroundImage = 'url(images/2.jpg)';
+});
+
+document.getElementById('cambiarImagen2').addEventListener('click', function() {
+  // Obtén el elemento al que deseas cambiar la imagen de fondo
+  var miElemento = document.getElementById('body');
+  // Cambia la propiedad background-image
+  miElemento.style.backgroundImage = 'url(images/1.jpg)';
+});
